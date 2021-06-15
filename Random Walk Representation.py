4e4755cbd50d7655b5939c5e73260be9ba9b9036
Raquel Romero <69012238+Raquel-RP@users.noreply.github.com>
2021-06-16 01:08:29 +0200

    Update Random Walk Representation.py

--- Random Walk Representation.py
+++ Random Walk Representation.py
@@ -16,8 +16,8 @@ n = int(input("Introduce the number of steps you want to represent: "))
 
 x_o = np.array([[0],[0]])                                           #Random Walk's two-dimensional initial conditions.
 
-delta_x = np.random.normal(0,1,(2,n))                               #The random positions are under a normal distribution with a mean equal to 0 and a standard deviation equal to 1.
-                                                                    #We create and array of (2,n) being n the number of steps introduced.
+delta_x = np.random.normal(0,1,(2,n))                               #Random positions are under a normal distribution (mean=0 : standard deviation=1).
+                                                                    #Array of (2,n) being n the number of steps introduced.
 
 x = np.concatenate((x_o, np.cumsum(delta_x, axis = 1)), axis =1)    #With consum we concatenate the sum of all the random values to create the walk.
 
